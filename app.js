e9a0fc8621a7d789943145e9d5b07d73855146ad
Blue-Grizzly <123905990+Blue-Grizzly@users.noreply.github.com>
2023-03-30 11:39:17 +0200

    codereview
    changes to code made following code review 033023

--- app.js
+++ app.js
@@ -4,24 +4,23 @@ window.addEventListener("load", start);
 
 async function start(){
  const pokemon = await getPokemon();
- pokemon.sort(sortBydexNumber);
+ pokemon.sort(sortByPokedexNumber);
  showAllPokemon(pokemon);
 }
 
-// Gets JSON data v
 async function getPokemon(){
     const response = await fetch("https://cederdorff.github.io/dat-js/05-data/pokemons.json");
     const data = await response.json();
     return data;
 }
 
-// Sorts pokemon by their Pokedex number v 
-function sortBydexNumber(pokemonA, pokemonB){
+function sortByPokedexNumber(pokemonA, pokemonB){
+    // Sorts pokemon by their Pokedex number v 
     return pokemonA.dexindex - pokemonB.dexindex;
 }
 
-// Creates the pokemon grid/list with the html in showPokemon for all pokemon in data
 function showAllPokemon(pokemonList) {
+    // Creates the pokemon grid/list with the html in showPokemon for all pokemon in data
     for (const pokemon of pokemonList) {
         showPokemon(pokemon);
     }
@@ -37,23 +36,68 @@ function showPokemon(pokemon) {
     </article>
     `;
     document.querySelector("#pokemonList").insertAdjacentHTML("beforeend", myHTML);
+    document.querySelector("#pokemonList article:last-child").classList.add(typeThemeSelector(pokemon.type));
     document.querySelector("#pokemonList article:last-child").addEventListener("click", clickPokemon);
     function clickPokemon(){
         document.querySelector("#detailView").showModal();
-        pokemonDialog(pokemon); 
+        displayPokemonData(pokemon); 
     }
 }
 
-// The engine room v
-function pokemonDialog(pokemon){
+function displayPokemonData(pokemon){
+// Dom manipulation 
 
-// Variables v
-    let typeTest = pokemon.type;
-    let evMessage
-    let type
-    
-    
-// The test for what type the clicked pokemon has to determine what color the background will be on the modal v
+    document.querySelector("#pokedexEntry").textContent = `${pokemon.description}`;
+    document.querySelector("#pokemonImage").src = `${pokemon.image}`;
+    // document.querySelector("#pokemonFootPrint").src = `pokemon.footprint`; removed due to copy right concerns.
+    document.querySelector("#pokemonName").textContent = `${pokemon.name}`;
+    document.querySelector("#dexIndex").textContent = `#${pokemon.dexindex}`;
+    document.querySelector("#ability").textContent = `Ability: ${pokemon.ability}`;
+    document.querySelector("#pokemonType").textContent = `Type: ${pokemon.type}`;
+    document.querySelector("#pokemonSubtype").textContent = checkSubtype(pokemon.subtype);    
+    document.querySelector("#weakness").textContent = `Weaknesses: ${pokemon.weaknesses}`;
+    document.querySelector("#gender").textContent = `${pokemon.gender}`;
+    document.querySelector("#weight").textContent = `Weight: ${pokemon.weight}g`;
+    document.querySelector("#height").textContent = `Height: ${pokemon.height}cm`;
+    document.querySelector("#generation").textContent = `Generation introduced: ${pokemon.generation}`;
+    document.querySelector("#gameVersion").textContent = `Game versions available: ${pokemon.spilversion}`;
+    document.querySelector("#canEvolve").textContent = checkCanEvolve(pokemon.canEvolve);
+    document.querySelector("#statHP").textContent = `Health: ${pokemon.statsHP}`;
+    document.querySelector("#statDefence").textContent = `Defence: ${pokemon.statsDefence}`;
+    document.querySelector("#statAttack").textContent = `Attack: ${pokemon.statsAttack}`;
+    document.querySelector("#statSpAttack").textContent = `SP Attack: ${pokemon.statsSpecialAttack}`;
+    document.querySelector("#statSpDefence").textContent = `SP Defence: ${pokemon.statsSpecialDefence}`;
+    document.querySelector("#statSpeed").textContent = `Speed: ${pokemon.statsSpeed}`;
+    document.querySelector("#statTotal").textContent = `Total: ${pokemon.statsAttack+pokemon.statsDefence+pokemon.statsHP+pokemon.statsSpecialAttack+pokemon.statsSpecialDefence+pokemon.statsSpeed}`;
+    document.querySelector("#detailView").classList.add(typeThemeSelector(pokemon.type));
+   
+// Waits for the modal to close so the background can be removed v
+    document.querySelector("#detailView").addEventListener("close", removeModalTheme);
+}
+
+//
+function checkSubtype(subtype){
+    if (subtype != undefined || subtype != null){
+        return`Subtype: ${subtype}`;
+    } else{
+        return;
+    }
+}
+
+function checkCanEvolve(canEvolve){
+    // Creates message for wether or not the pokemon can be evolved
+    if (canEvolve === true){
+        return `This pokémon can  evolve.`;
+    } else if(canEvolve === false){
+        return `This pokémon can not evolve.`;
+    }
+}
+
+function typeThemeSelector(pokeType){
+    // The test for what type the clicked pokemon has to determine what color the background will be on the modal
+    let typeTest = pokeType;
+    let type;
+
     if(typeTest.includes("Fire") === true || typeTest.includes("fire") === true){
         type = "Fire";
     } else if(typeTest.includes("Water") === true || typeTest.includes("water") === true) {
@@ -90,57 +134,16 @@ function pokemonDialog(pokemon){
         type = "Steel";
     } else if(typeTest.includes("Fairy") === true || typeTest.includes("fairy") === true) {
         type = "Fairy";
-    }
-    
-    let typeTheme = `${type}Theme`;
-    
-// Creates message for wether or not the pokemon can be evolved v
-    if (pokemon.canEvolve === true){
-        evMessage = `This pokémon can  evolve.`;
-    } else if(pokemon.canEvolve === false){
-        evMessage = `This pokémon can not evolve.`;
-    }
-// Adds up the stats for the total stats data point v
-    const calculatedStats = pokemon.statsAttack+pokemon.statsDefence+pokemon.statsHP+pokemon.statsSpecialAttack+pokemon.statsSpecialDefence+pokemon.statsSpeed;
-
-// Dom manipulation v
-    document.querySelector("#detailView").classList.add(typeTheme);
-    document.querySelector("#pokedexEntry").textContent = `${pokemon.description}`;
-    document.querySelector("#pokemonImage").src = `${pokemon.image}`;
-    // document.querySelector("#pokemonFootPrint").src = `pokemon.footprint`; removed due to copy right concerns.
-    document.querySelector("#pokemonName").textContent = `${pokemon.name}`;
-    document.querySelector("#dexIndex").textContent = `#${pokemon.dexindex}`;
-    document.querySelector("#ability").textContent = `Ability: ${pokemon.ability}`;
-    document.querySelector("#pokemonType").textContent = `Type: ${pokemon.type}`;
-    
-    if (pokemon.subtype != undefined || pokemon.subtype != null){
-        document.querySelector("#pokemonSubtype").textContent = `Subtype: ${pokemon.subtype}`;
     } else{
-        document.querySelector("#pokemonSubtype").textContent = "";
+        type = "misinput"
     }
-    
-    document.querySelector("#weakness").textContent = `Weaknesses: ${pokemon.weaknesses}`;
-    document.querySelector("#gender").textContent = `${pokemon.gender}`;
-    document.querySelector("#weight").textContent = `Weight: ${pokemon.weight}g`;
-    document.querySelector("#height").textContent = `Height: ${pokemon.height}cm`;
-    document.querySelector("#generation").textContent = `Generation introduced: ${pokemon.generation}`;
-    document.querySelector("#gameVersion").textContent = `Game versions available: ${pokemon.spilversion}`;
-    document.querySelector("#canEvolve").textContent = evMessage;
-    document.querySelector("#statHP").textContent = `Health: ${pokemon.statsHP}`;
-    document.querySelector("#statDefence").textContent = `Defence: ${pokemon.statsDefence}`;
-    document.querySelector("#statAttack").textContent = `Attack: ${pokemon.statsAttack}`;
-    document.querySelector("#statSpAttack").textContent = `SP Attack: ${pokemon.statsSpecialAttack}`;
-    document.querySelector("#statSpDefence").textContent = `SP Defence: ${pokemon.statsSpecialDefence}`;
-    document.querySelector("#statSpeed").textContent = `Speed: ${pokemon.statsSpeed}`;
-    document.querySelector("#statTotal").textContent = `Total: ${calculatedStats}`;
 
-// Waits for the modal to close so the background can be removed v
-    document.querySelector("#detailView").addEventListener("close", closedModal);
+ return `${type}Theme`;
 }
 
 
-function closedModal(){
+function removeModalTheme(){
     document.querySelector("#detailView").classList.remove
-        ("NormalTheme", "FireTheme", "WaterTheme", "GrassTheme", "ElectricTheme", "IceTheme", "FightingTheme", "PoisonTheme", "GroundTheme", "FlyingTheme", "PsychicTheme", "BugTheme", "RockTheme", "GhostTheme", "DarkTheme", "DragonTheme", "SteelTheme", "FairyTheme");
+        ("NormalTheme", "FireTheme", "WaterTheme", "GrassTheme", "ElectricTheme", "IceTheme", "FightingTheme", "PoisonTheme", "GroundTheme", "FlyingTheme", "PsychicTheme", "BugTheme", "RockTheme", "GhostTheme", "DarkTheme", "DragonTheme", "SteelTheme", "FairyTheme", "misinputTheme");
     //Removes all the themes ^
 }
